7b9e7025f36d7c27fe8043a3a91592e2b23d64fc
Reddy <2389606@cognizant.com>
2025-03-05 09:30:56 +0530

    this is our first commit

---
+++ src/main/java/com/project/InventoryManagementSystemCtsApplication.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManagementSystemCtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryManagementSystemCtsApplication.class, args);
+	}
+
+}
