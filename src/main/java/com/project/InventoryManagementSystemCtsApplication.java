3426b2c6e410744174e1cc7cdfe261f29ef45e4a
Hasitha <2389451@cognizant.com>
2025-03-06 11:36:27 +0530

    hasitha's project

--- src/main/java/com/project/InventoryManagementSystemCtsApplication.java
+++ src/main/java/com/project/InventoryManagementSystemCtsApplication.java
@@ -9,7 +9,8 @@ public class InventoryManagementSystemCtsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryManagementSystemCtsApplication.class, args);
-		System.out.print("harsha ikkada");
+		System.out.println("harsha ikkada");
+		
 	}
 
 }
