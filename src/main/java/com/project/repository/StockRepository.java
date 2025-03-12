f720366eab0d00aee91de2a7b20ea6e385c5b58e
J <2389562@cognizant.com>
2025-03-12 16:53:33 +0530

    benii module updated

---
+++ src/main/java/com/project/repository/StockRepository.java
@@ -0,0 +1,19 @@
+package com.project.repository;
+
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.project.entity.Stock;
+
+@Repository
+public interface StockRepository extends JpaRepository<Stock, Integer> {
+	List<Stock> findByReorderLevel(String reorderLevel);
+
+	@Query(value = "Select * from stocks order by quantity desc", nativeQuery = true)
+	List<Object[]> findHighestQuantity(Pageable pageable);
+
+}
--- src/main/java/com/project/service/OrderService.java
+++ src/main/java/com/project/service/OrderService.java
@@ -14,48 +14,52 @@ import com.project.repository.OrderRepository;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+
 @Service
 public class OrderService {
 	@Autowired
-private OrderRepository orderRepo;
-	public void create(Order order)
-	{
+	private OrderRepository orderRepo;
+
+	public void create(Order order) {
 		orderRepo.save(order);
 	}
-	public Order getOrderById(int id)
-	{
+
+	public Order getOrderById(int id) {
 		return orderRepo.findByOrderId(id);
 	}
-public Optional<Order>getOrderByProductName(String name)
-{ 
-	return orderRepo.findByProductName(name);
-}
-public Optional<Order>getOrderByStatus(String status)
-{
-	return orderRepo.findByStatus(status);
-}
-public void updateOrderStatus(int id,String status) {
-	Order o=orderRepo.findByOrderId(id);
-	if(o!=null)
-	{
-		o.setStatus(status);
-	}
-	orderRepo.save(o);
-}
-public List<Order> getOrderByDate(LocalDate startDate, LocalDate endDate) {
-	return orderRepo.getOrderByDate(startDate,endDate);
-}
 
-public List<Object[]> getHighestOrderedProduct() {
-	Pageable pageable=PageRequest.of(0, 1);
-	return orderRepo.findHighestOrderedProduct(pageable);
-}
-public String deleteByOrderId(int id) {
-orderRepo.deleteById(id);
-	return "Successfully deleted";
-}
-public String deleteAll() {
-	orderRepo.deleteAll();
-	return "Deleted All entries";
-}
+	public Optional<Order> getOrderByProductName(String name) {
+		return orderRepo.findByProductName(name);
+	}
+
+	public Optional<Order> getOrderByStatus(String status) {
+		return orderRepo.findByStatus(status);
+	}
+
+	public void updateOrderStatus(int id, String status) {
+		Order o = orderRepo.findByOrderId(id);
+		if (o != null) {
+			o.setStatus(status);
+		}
+		orderRepo.save(o);
+	}
+
+	public List<Order> getOrderByDate(LocalDate startDate, LocalDate endDate) {
+		return orderRepo.getOrderByDate(startDate, endDate);
+	}
+
+	public List<Object[]> getHighestOrderedProduct() {
+		Pageable pageable = PageRequest.of(0, 1);
+		return orderRepo.findHighestOrderedProduct(pageable);
+	}
+
+	public String deleteByOrderId(int id) {
+		orderRepo.deleteById(id);
+		return "Successfully deleted";
+	}
+
+	public String deleteAll() {
+		orderRepo.deleteAll();
+		return "Deleted All entries";
+	}
 }
